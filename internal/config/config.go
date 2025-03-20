73d933687b7f377db2da610fe964afdfa2358822
Mohammed Dadi <artofmohammed@gmail.com>
2025-03-20 16:15:21 +0100

    feat(commands, config ): extend config and commands functionality
    Added commands struct and handler login in main function and Read function in config package
    These changes help to read from and write to the config file while handling login commands.

--- internal/config/config.go
+++ internal/config/config.go
@@ -6,62 +6,28 @@ import (
 	"os"
 )
 
-const config_filename = "/.gatorconfig.json"
+const configFilename = "/.gatorconfig.json"
 
 type Config struct {
-	DbURL             string `json:"db_url"`
-	Current_user_name string `json:"current_user_name"`
+	DbURL           string `json:"db_url"`
+	CurrentUserName string `json:"CurrentUserName"`
 }
 
 func (c *Config) SetUser(username string) error {
-	c.Current_user_name = username
-	write(*c)
-	return nil
+	c.CurrentUserName = username
+	return write(c)
 
 }
 
-func Read() (Config, error) {
-	config_file_path, err := getConfigFilePath()
-	if err != nil {
-		fmt.Printf("error getting config file path: %v", err)
-		return Config{}, err
-	}
-
-	file, err := os.Open(config_file_path)
-	if err != nil {
-		fmt.Printf("error opening config file: %v", config_file_path)
-		return Config{}, err
-	}
-	defer file.Close()
-
-	config_struct := Config{}
-	decoder := json.NewDecoder(file)
-	if err = decoder.Decode(&config_struct); err != nil {
-		fmt.Printf("error decoding request body, error: %v", err)
-		return Config{}, err
-	}
-	return config_struct, nil
-}
-
-func getConfigFilePath() (string, error) {
-	home_path, err := os.UserHomeDir()
-	if err != nil {
-		fmt.Printf("invalid home path")
-		return "", fmt.Errorf("home path error")
-	}
-	config_file_path := home_path + config_filename
-	return config_file_path, nil
-}
-
-func write(cfg Config) error {
+func write(cfg *Config) error {
 
 	//open the file first
-	config_file_path, err := getConfigFilePath()
+	configFilePath, err := getConfigFilePath()
 	if err != nil {
 		fmt.Printf("error geting config file path, %v", err)
 		return err
 	}
-	file, err := os.OpenFile(config_file_path, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0644)
+	file, err := os.OpenFile(configFilePath, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0644)
 	if err != nil {
 		fmt.Printf("Error opening file: %v", err)
 		return err
@@ -75,4 +41,37 @@ func write(cfg Config) error {
 		return err
 	}
 	return nil
+}
+
+func Read() (Config, error) {
+	configFilePath, err := getConfigFilePath()
+	if err != nil {
+		fmt.Printf("error getting config file path: %v", err)
+		return Config{}, err
+	}
+
+	file, err := os.Open(configFilePath)
+	if err != nil {
+		fmt.Printf("error opening config file: %v", configFilePath)
+		return Config{}, err
+	}
+	defer file.Close()
+
+	configStruct := Config{}
+	decoder := json.NewDecoder(file)
+	if err = decoder.Decode(&configStruct); err != nil {
+		fmt.Printf("error decoding request body, error: %v", err)
+		return Config{}, err
+	}
+	return configStruct, nil
+}
+
+func getConfigFilePath() (string, error) {
+	homePath, err := os.UserHomeDir()
+	if err != nil {
+		fmt.Printf("invalid home path")
+		return "", fmt.Errorf("home path error")
+	}
+	configFilePath := homePath + configFilename
+	return configFilePath, nil
 }
--- main.go
+++ main.go
@@ -2,27 +2,85 @@ package main
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/hamstimusprime/blog_aggregator/internal/config"
 )
 
 func main() {
+	//read config file from disk and store in cfg
 	cfg, err := config.Read()
 	if err != nil {
 		fmt.Printf("error reading config file: %v", err)
 		return
 	}
 
-	if err := cfg.SetUser("lane"); err != nil {
-		fmt.Printf("error setting user: %v", err)
+	//store config in a state struct
+	s := state{
+		config: &cfg,
 	}
 
-	updated_cfg, err := config.Read()
-	if err != nil {
-		fmt.Printf("error reading config file: %v", err)
-		return
+	//create instance of commands and store handlers into it
+	cmds := commands{
+		handlersMap: make(map[string]func(*state, command) error),
 	}
-	fmt.Println(updated_cfg.DbURL)
-	fmt.Println(updated_cfg.Current_user_name)
+	cmds.register("login", handlerLogin)
 
+	//get the input from the command line when program runs
+	commandLineInput := os.Args
+	if len(commandLineInput) < 3 {
+		fmt.Println("error, no arguments provided")
+		os.Exit(1)
+	}
+	if len(commandLineInput) > 3 {
+		fmt.Println("warning! multiple arguments provided, only first argument would be used")
+	}
+	commandName := commandLineInput[1]
+	commandArgs := commandLineInput[2:]
+	cmd := command{name: commandName, args: commandArgs}
+
+	//call command with arguments
+	if err = cmds.run(&s, cmd); err != nil {
+		fmt.Println("could not execute run command")
+		os.Exit(1)
+	}
+}
+
+type state struct {
+	config *config.Config
+}
+
+func handlerLogin(s *state, cmd command) error {
+	if len(cmd.args) == 0 {
+		return fmt.Errorf("expected arguments. No arguments provided")
+	}
+
+	if err := s.config.SetUser(cmd.args[0]); err != nil {
+		fmt.Printf("unable to set user name, %v", err)
+		return err
+	}
+	fmt.Println("username has been set")
+	return nil
+}
+
+type command struct {
+	name string
+	args []string
+}
+
+type commands struct {
+	handlersMap map[string]func(*state, command) error
+}
+
+func (c *commands) register(name string, f func(*state, command) error) {
+	c.handlersMap[name] = f
+}
+
+func (c *commands) run(s *state, cmd command) error {
+	handler, ok := c.handlersMap[cmd.name]
+	if !ok {
+		return fmt.Errorf("invalid command")
+	}
+	handler(s, cmd)
+	return nil
 }
